4920f8da960222ab8601c248ab7b8ff57af19275
Pace2Car <Pace2Car@163.com>
2019-08-23 17:57:27 +0800

    14、20

---
+++ algorithm/src/easy/string/LongestCommonPrefix.java
@@ -0,0 +1,61 @@
+package easy.string;
+
+/**
+ * 14.最长公共前缀 -- 解题失败
+ * 编写一个函数来查找字符串数组中的最长公共前缀。
+ *
+ * 如果不存在公共前缀，返回空字符串 ""。
+ * @author chenjiahao
+ * @date 2019/8/23 16:55
+ */
+public class LongestCommonPrefix {
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+
+        String[] strs = new String[]{"aflower","bflow","cflight"};
+//        String[] strs = new String[]{"a"};
+//        String[] strs = new String[]{"c", "c"};
+
+        System.out.println(longestCommonPrefixAns(strs));
+
+        System.out.println(System.currentTimeMillis() - start + "ms");
+    }
+
+    /**
+     * 我的错误答案
+     * @param strs
+     * @return
+     */
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        if (strs.length == 1) return strs[0];
+        int commonLength = 0;
+        for (int i = 1; i < strs.length; i++) {
+            for (int j = 0; j < strs[0].length() && j < strs[i].length(); j++) {
+                if (strs[i].charAt(j) != strs[0].charAt(j)) {
+                    commonLength = j;
+                } else if (strs[i].charAt(j) == strs[0].charAt(j)){
+                    commonLength++;
+                }
+            }
+        }
+        return strs[0].substring(0,commonLength);
+    }
+
+    /**
+     * 标准答案
+     * @param strs
+     * @return
+     */
+    public static String longestCommonPrefixAns(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        for (int i = 0; i < strs[0].length() ; i++){
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j ++) {
+                if (i == strs[j].length() || strs[j].charAt(i) != c)
+                    return strs[0].substring(0, i);
+            }
+        }
+        return strs[0];
+    }
+}
---
+++ algorithm/src/easy/string/ValidParenthesis.java
@@ -0,0 +1,60 @@
+package easy.string;
+
+import java.util.Stack;
+
+/**
+ * 20.有效的括号
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ *
+ * 有效字符串需满足：
+ *     1.左括号必须用相同类型的右括号闭合。
+ *     2.左括号必须以正确的顺序闭合。
+ *
+ * @author chenjiahao
+ * @date 2019/8/23 17:53
+ */
+public class ValidParenthesis {
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+
+//        String s = "([)]";
+        String s = "()[]{}{{}}(-  -)";
+//        String s = "{[]}";
+//        String s = "]";
+        System.out.println(isValid(s));
+
+        System.out.println(System.currentTimeMillis() - start + "ms");
+    }
+
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        char[] chars = s.toCharArray();
+        for (char c : chars) {
+            switch (c) {
+                case '(' :
+                case '{' :
+                case '[' :
+                    stack.push(c);
+                    break;
+                case ')' :
+                    if (stack.empty() || stack.pop() != '(') {
+                        return false;
+                    }
+                    break;
+                case '}' :
+                    if (stack.empty() || stack.pop() != '{') {
+                        return false;
+                    }
+                    break;
+                case ']' :
+                    if (stack.empty() || stack.pop() != '[') {
+                        return false;
+                    }
+                    break;
+                default  :
+                    break;
+            }
+        }
+        return stack.empty();
+    }
+}
